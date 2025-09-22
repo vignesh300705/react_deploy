4aa319a8cd31e99c5ec38ba028e6995f0a5898be
vignesh300705 <vigneshbalaji300705@gmail.com>
2025-09-22 11:18:13 +0530

    updated changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>HI,Welcome To your First Deployment React Application.</h1>
+      <h1>HI,Welcome To your First Deployment React Application. I am a Mern Stack Developer </h1>
     </div>
   );
 }
